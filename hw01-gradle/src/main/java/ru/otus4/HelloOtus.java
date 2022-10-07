2bc555d4f4cda508ed59450f8c9409d16b39fcc5
ivaschenko <vaschenko@omnicomm.ru>
2022-10-07 14:21:34 +0300

    hw01-gradle

---
+++ hw01-gradle/src/main/java/ru/otus4/HelloOtus.java
@@ -0,0 +1,11 @@
+package ru.otus4;
+
+import com.google.common.base.Strings;
+
+public class HelloOtus {
+
+    public static boolean isNullOrEmpty(String str) {
+        return Strings.isNullOrEmpty(str);
+    }
+
+}
