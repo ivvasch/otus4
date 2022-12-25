85c5a9139c7d81b68626637f548cc993fa46bb88
ivaschenko <vaschenko@omnicomm.ru>
2022-12-25 19:32:21 +0300

    hw-16-JSON

---
+++ hw-16-JSON/src/main/java/ru/otus4/dataprocessor/FileSerializer.java
@@ -0,0 +1,33 @@
+package ru.otus4.dataprocessor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class FileSerializer implements Serializer {
+
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void serialize(Map<String, Double> data) {
+        //формирует результирующий json и сохраняет его в файл
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String outFile = getFileName();
+            mapper.writeValue(new File(outFile), data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+}
