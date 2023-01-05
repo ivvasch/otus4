67cdae09302fb23918a33c5a9d311c55778926a7
ivaschenko <vaschenko@omnicomm.ru>
2023-01-05 11:24:08 +0300

    hw-18-JDBC

---
+++ hw-18-JDBC/src/main/java/ru/otus4/jdbc/mapper/EntitySQLMetaData.java
@@ -0,0 +1,14 @@
+package ru.otus4.jdbc.mapper;
+
+/**
+ * Создает SQL - запросы
+ */
+public interface EntitySQLMetaData {
+    String getSelectAllSql();
+
+    String getSelectByIdSql();
+
+    String getInsertSql();
+
+    String getUpdateSql();
+}
