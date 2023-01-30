fc82ea192d0ab74b6c8a319cade55397c04ea27a
ivaschenko <vaschenko@omnicomm.ru>
2023-01-30 23:00:26 +0300

    hw22-Cache

--- hw22-Cache/src/main/java/ru/otus4/cachehw/MyCache.java
+++ hw22-Cache/src/main/java/ru/otus4/cachehw/MyCache.java
@@ -1,19 +1,29 @@
 package ru.otus4.cachehw;
 
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.WeakHashMap;
 
 public class MyCache<K, V> implements HwCache<K, V> {
     //Надо реализовать эти методы
     WeakHashMap<K, V> cache = new WeakHashMap<>();
 
+    List<HwListener<K, V>> listeners = new LinkedList<>();
+
     @Override
     public void put(K key, V value) {
+        for (HwListener<K, V> listener : listeners) {
+            listener.notify(key, value, "PUT");
+        }
         cache.put(key, value);
     }
 
     @Override
     public void remove(K key) {
+        for (HwListener<K, V> listener : listeners) {
+            listener.notify(key, cache.get(key), "REMOVE");
+        }
         cache.remove(key);
     }
 
@@ -28,11 +38,12 @@ public class MyCache<K, V> implements HwCache<K, V> {
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
+        listeners.remove(listener);
 
     }
 }
